e6deab450718b5e35e186be084aa00cea4634f4b
sodiel <detopuse@gmail.com>
2026-03-06 18:50:10 +0300

    production build

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -47,33 +48,31 @@ app.delete("/api/notes/:id", (request, response) => {
   console.log(notes);
 });
 
-
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
